0547598d36fb815ebfc52fec342a5c4c80ad63c6
imrk974 <imrk974@github.com>
2020-01-19 20:10:44 +0530

    Check sum of three num in array

---
+++ src/main/java/com/DailyCodingProblem/solution/CheckSumofThreeNumInArray.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.DailyCodingProblem.solution;
+
+/**
+ * @author rakkumar
+ *
+ */
+public class CheckSumofThreeNumInArray {
+
+	
+	private static boolean checkThreeSumExistinArray(int[] arr, int sum) {
+			
+		//Iterative Approach, complexity O(n^3) - Not recommended
+		long startTime = System.nanoTime();
+		long elapsedTime;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				for (int k = j; k < arr.length; k++) {
+					if (arr[i]+arr[j]+arr[k] == sum) {
+						elapsedTime = System.nanoTime() - startTime;
+						System.out.println("Total time taken to execute : " + elapsedTime);
+						return true;
+					}
+					
+				}
+			}
+		}
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("Total time taken to execute : " + elapsedTime);
+		return false;
+	}
+	
+	private static boolean checkThreeSumExistinArrayOptimal(int[] arr, int sum) {
+		long startTime = System.nanoTime();
+		long elapsedTime;
+		for (int i = 0; i < arr.length-1; i++) {
+			int reqSum = sum - arr[i];
+			for (int j=i+1; j < arr.length-1 ; j++) {
+				int reqnum = reqSum - arr[j];
+				if (reqnum == arr[j + 1]) {
+					elapsedTime = System.nanoTime() - startTime;
+					System.out.println("Total time taken to execute : " + elapsedTime);
+					return true;
+				}
+			}
+		}
+		
+		elapsedTime = System.nanoTime() - startTime;
+		System.out.println("Total time taken to execute : " + elapsedTime);
+		return false;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		int[] arr = { 11, 15, 3, 7, 34, 6, 2, 8, 11, 3, 32, 45, 689, 32, 7, 50, 80, 73 };
+
+		System.out.println(checkThreeSumExistinArrayOptimal(arr, 203));
+		System.out.println("******************************");
+		System.out.println(checkThreeSumExistinArray(arr, 103));
+		
+		
+
+	}
+
+}
