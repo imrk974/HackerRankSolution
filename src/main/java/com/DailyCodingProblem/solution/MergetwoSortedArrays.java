d766de98d87c48ed3ba4b15fa470907765aaf7d3
imrk974 <imrk974@github.com>
2020-01-27 15:34:30 +0530

    merged array code modified

--- src/main/java/com/DailyCodingProblem/solution/MergetwoSortedArrays.java
+++ src/main/java/com/DailyCodingProblem/solution/MergetwoSortedArrays.java
@@ -4,38 +4,36 @@ import java.util.Arrays;
 
 public class MergetwoSortedArrays {
 
-	
 	public static int[] mergeTwoSortedArray(int[] arrayOne, int[] arrayTwo) {
-		int[] mergedArray = new int[arrayOne.length+arrayTwo.length];
-		
-		int oneIndex=0, twoIndex=0, mergeIndex=0;
-		int i=0;
-		while (mergeIndex < (arrayOne.length + arrayTwo.length)-1) {
-			
-			if(arrayOne[oneIndex] <= arrayTwo[twoIndex]) {
+		int[] mergedArray = new int[arrayOne.length + arrayTwo.length];
+
+		int oneIndex = 0, twoIndex = 0, mergeIndex = 0;
+
+		while (oneIndex < arrayOne.length || twoIndex < arrayTwo.length) {
+
+			if (oneIndex < arrayOne.length && twoIndex < arrayTwo.length) {
+				if (arrayOne[oneIndex] <= arrayTwo[twoIndex]) {
+					mergedArray[mergeIndex] = arrayOne[oneIndex];
+					oneIndex++;
+				} else {
+					mergedArray[mergeIndex] = arrayTwo[twoIndex];
+					twoIndex++;
+				}
+			} else if (oneIndex < arrayOne.length) {
 				mergedArray[mergeIndex] = arrayOne[oneIndex];
 				oneIndex++;
-			} else {
+			} else if (twoIndex < arrayTwo.length) {
 				mergedArray[mergeIndex] = arrayTwo[twoIndex];
 				twoIndex++;
 			}
-			
-			if(oneIndex == arrayOne.length-1) {
-				
-			} else if(twoIndex == arrayTwo.length-1) {
-				
-			}
-			
-			mergeIndex++;	
-		}		
-		
+			mergeIndex++;
+		}
 		return mergedArray;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		int[] arrayOne= {1,5};
-		int[] arrayTwo= {2,4};
+		int[] arrayOne = { 1, 5 };
+		int[] arrayTwo = { 2, 4 };
 
 		System.out.println(Arrays.toString(mergeTwoSortedArray(arrayOne, arrayTwo)));
 	}
